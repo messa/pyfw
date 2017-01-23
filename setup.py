db44d718400e720906bbdf81be1591368d2d2314
Petr Messner <petr.messner@gmail.com>
2017-01-23 17:09:13 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfw',
-    version='0.1.0',
+    version='0.1.1',
     description='Python Firewall',
     author='Petr Messner',
     author_email='petr.messner@gmail.com',
